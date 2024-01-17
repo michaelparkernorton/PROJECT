74e0c54160c7d4808e7ce1997b94ed8ca700eb05
michaelparkernorton <michaelparkernorton.va@gmail.com>
2024-01-17 14:48:09 -0300

    updates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe', 
+      'This is a test recipe', 
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSf_ST0jazyb8_nuJzOPcEVFV5vtCfd57RohzkVMGlURmbuaFCA-EnFTmtF60A3Bf-sRY&usqp=CAU'
+    ),
+    new Recipe(
+      'Test recipe', 
+      'This is a test recipe', 
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSf_ST0jazyb8_nuJzOPcEVFV5vtCfd57RohzkVMGlURmbuaFCA-EnFTmtF60A3Bf-sRY&usqp=CAU'
+    )
+  ];
 }
